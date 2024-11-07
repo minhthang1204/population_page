7274e1ab5e583af0dd5ae11398fc76506ea38adc
minhthang1204 <thangkt0008@gmail.com>
2024-11-07 17:26:16 +0700

    add map

--- src/views/admin/default/components/MapChart.tsx
+++ src/views/admin/default/components/MapChart.tsx
@@ -6,32 +6,34 @@ const geoUrl = '/diaphantinhenglish.geojson';
 
 // Dữ liệu dân số (ví dụ)
 const populationData: any = {
-  'Hà Nội': 8000000,
-  'TP.HCM': 9000000,
+  'Ha Noi': 8000000,
+  'Cao Bang': 9000000,
   // Các tỉnh khác...
 };
 
 const MapChart = () => {
   return (
-    <div style={{ width: '100%', height: 'auto' }}>
       <ComposableMap
         projection="geoMercator"
-        style={{ width: '100%', height: '100%' }}
+        projectionConfig={{
+          scale: 2500, // Tăng giá trị để phóng to bản đồ
+          center: [105.85, 21.0285], // Tâm bản đồ, có thể chỉnh để di chuyển vị trí bản đồ
+        }}
       >
-        <Geographies geography={geoUrl}>
+        <Geographies geography={geoUrl} >
           {({ geographies }) =>
             geographies.map((geo) => {
               const provinceName = geo.properties.Name; // Giả sử thuộc tính tên tỉnh là 'name'
               const population = populationData[provinceName] || 0;
               const fillColor = population > 5000000 ? '#3366cc' : '#99ccff'; // Điều chỉnh màu dựa trên dân số
-
+              console.log(populationData[provinceName])
               return (
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
                   fill={fillColor}
                   style={{
-                    default: { outline: 'none' },
+                    default: { outline: 'none', },
                     hover: { fill: '#ffa726', outline: 'none' },
                     pressed: { fill: '#ff7043', outline: 'none' },
                   }}
@@ -41,7 +43,6 @@ const MapChart = () => {
           }
         </Geographies>
       </ComposableMap>
-    </div>
   );
 };
 
